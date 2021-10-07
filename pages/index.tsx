0c23066d32d521b16aea059467b13fd715631811
chaudev <soc900k@mail.com>
2021-10-08 04:44:56 +0700

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
+
+  const onPressButton = (): void => {
+    router.push("/test");
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -22,33 +30,14 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+          <div
+            onClick={() => {
+              onPressButton();
+            }}
+            className={styles.button}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <p>Click Me</p>
+          </div>
         </div>
       </main>
 
